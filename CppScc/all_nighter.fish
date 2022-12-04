5c5277191a06ef99ad5016ee0207a662bab88221
isidorostsa <tsa.isidoros@gmail.com>
2022-12-04 16:48:22 +0200

    made script more customizable

--- CppScc/all_nighter.fish
+++ CppScc/all_nighter.fish
@@ -1,4 +1,4 @@
-function all_nighter --argument times --description="A script to take build all implementations and run them for different thread counts, expected to take many hours"
+function all_nighter --argument times threads_start threads_step threads_end  --description="A script to take build all implementations and run them for different thread counts, expected to take many hours"
     cd ~/Desktop/pdsProject/CppScc
 
     mkdir -p results/serial results/OpenMP results/OpenCilk results/pthread
@@ -16,7 +16,7 @@ function all_nighter --argument times --description="A script to take build all 
     cd OpenMP
     make clean
     make
-    for i in (seq 2 2 12)
+    for i in (seq $threads_start $threads_step $threads_end)
         export OMP_NUM_THREADS=$i
         echo "Running with $i threads"
         ./colorSCC ../../matrices $times 0 1 | tee ../results/OpenMP/threads_$i.txt
@@ -36,7 +36,7 @@ function all_nighter --argument times --description="A script to take build all 
     cd OpenCilk
     make clean
     make
-    for i in (seq 2 2 12)
+    for i in (seq $threads_start $threads_step $threads_end)
         export CILK_NWORKERS=$i
         echo "Running with $i threads"
         ./colorSCC ../../matrices $times 0 1 | tee ../results/OpenCilk/threads_$i.txt
@@ -48,14 +48,11 @@ function all_nighter --argument times --description="A script to take build all 
     cd pthread
     make clean
     make
-    for i in (seq 2 2 12)
+    for i in (seq $threads_start $threads_step $threads_end)
         echo "Running with $i threads"
         ./colorSCC ../../matrices $times 0 1 $i | tee ../results/pthread/threads_$i.txt
     end
     cd ..
-
-
-
     #IF done before i check the results, keep it up for more threads
 
     # Make measurements for the OpenMP version
@@ -63,7 +60,8 @@ function all_nighter --argument times --description="A script to take build all 
     cd OpenMP
     make clean
     make
-    for i in (seq 12 2 24)
+    // start from threads_end + threads_step
+    for i in (seq (math $threads_end + $threads_step) $threads_step (math $threads_end*2))
         export OMP_NUM_THREADS=$i
         echo "Running with $i threads"
         ./colorSCC ../../matrices $times 0 1 | tee ../results/OpenMP/threads_$i.txt
@@ -75,7 +73,7 @@ function all_nighter --argument times --description="A script to take build all 
     cd OpenCilk
     make clean
     make
-    for i in (seq 12 2 24)
+    for i in (seq (math $threads_end + $threads_step) $threads_step (math $threads_end*2))
         export CILK_NWORKERS=$i
         echo "Running with $i threads"
         ./colorSCC ../../matrices $times 0 1 | tee ../results/OpenCilk/threads_$i.txt
@@ -87,7 +85,7 @@ function all_nighter --argument times --description="A script to take build all 
     cd pthread
     make clean
     make
-    for i in (seq 12 2 24)
+    for i in (seq (math $threads_end + $threads_step) $threads_step (math $threads_end*2))
         echo "Running with $i threads"
         ./colorSCC ../../matrices $times 0 1 $i | tee ../results/pthread/threads_$i.txt
     end
